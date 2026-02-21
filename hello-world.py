2eb3eaaaede6f1e7274a1e0b90ed87f879cf93f4
Kj Daley <daleyantics@gmail.com>
2026-02-21 01:14:00 -0400

    add more comments

--- hello-world.py
+++ hello-world.py
@@ -1,3 +1,5 @@
 # This is a python implementation of a Hello World! program.
+
+# This is so funky, editing a file before I make anoher change but retrospectively
 print('Hello World!')
 print('Goodby World!')
