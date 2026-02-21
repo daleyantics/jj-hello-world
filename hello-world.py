fc61d6539c8830da5ad132f3739e88529116a1c0
Kj Daley <daleyantics@gmail.com>
2026-02-21 01:23:35 -0400

    create hello and goodbye functions

--- hello-world.py
+++ hello-world.py
@@ -1,4 +1,12 @@
 # This is a python implementation of a Hello World! program.
 
 # This is so funky, editing a file before I make anoher change but retrospectively
-print('Hello World!')
+def print_hello():
+    print('Hello, World!')
+
+def print_goodbye():
+    print('Goodbye, World!')
+
+
+print_hello()
+print_goodbye()
